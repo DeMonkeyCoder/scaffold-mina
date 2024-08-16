6f8189b8fb6a4046384c400e9956a5f117dfe5fd
TheMonkeyCoder <themonkeycoder@proton.me>
2024-08-16 15:01:46 +0300

    feat: Connect Wallet button ui

--- packages/ui/src/components/ConnectWallet.tsx
+++ packages/ui/src/components/ConnectWallet.tsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/Home.module.css";
 import { useMinaWallet } from "@/hooks/useMinaWallet";
 import { formatPublicKey } from "@/utils";
-
+import Image from "next/image";
 
 export default function ConnectWallet() {
   const { connect, hasWallet, isConnected, account } = useMinaWallet();
@@ -11,23 +11,28 @@ export default function ConnectWallet() {
         href="https://www.aurowallet.com"
         target="_blank"
         rel="noreferrer"
-        className={styles.card}
+        className={`${styles.card} flex items-center justify-center`}
       >
         Install Auro Wallet
       </a>
     );
   }
-  
-  if (!isConnected) {
-    return ( 
-      <button className={`${styles.card} flex items-center justify-center`} onClick={connect}>
-        <img src="/assets/wallet-2.svg" alt="" />
-    Connect Wallet
-      </button>
-    );
-  }
-  if (account) {
-    return <button className={styles.card}>{formatPublicKey(account)}</button>;
-  }
-  return <button className={styles.card}>No Account</button>;
+
+  return (
+    <button
+      className={`${styles.card} flex items-center justify-center`}
+      onClick={() => {
+        if (!isConnected) {
+          connect();
+        }
+      }}
+    >
+      <Image width={16} height={16} src="/assets/wallet-2.svg" alt="" />
+      {isConnected
+        ? account
+          ? formatPublicKey(account)
+          : "No Account"
+        : "Connect Wallet"}
+    </button>
+  );
 }
