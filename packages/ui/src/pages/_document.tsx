f3f876875e976b3d1424d12e8628468779609793
TheMonkeyCoder <themonkeycoder@proton.me>
2024-08-18 03:29:51 +0300

    feat: add page title

--- packages/ui/src/pages/_document.tsx
+++ packages/ui/src/pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Mina Scaffold</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
