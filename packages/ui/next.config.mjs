22c57123219b149e2b3c9b7f44ab0f24df5709ce
TheMonkeyCoder <themonkeycoder@proton.me>
2024-08-18 04:12:42 +0300

    feat: add static export to nextjs configuration

--- packages/ui/next.config.mjs
+++ packages/ui/next.config.mjs
@@ -1,5 +1,5 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -7,15 +7,17 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 
   webpack(config, { isServer }) {
     if (!isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        o1js: path.resolve(__dirname, 'node_modules/o1js/dist/web/index.js'),
+        o1js: path.resolve(__dirname, "node_modules/o1js/dist/web/index.js"),
       };
     }
     config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.optimization.minimizer = [];
     return config;
   },
   // To enable o1js for the web, we must set the COOP and COEP headers.
@@ -23,20 +25,24 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
           },
           {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
           },
         ],
       },
     ];
   },
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
 };
 
 export default nextConfig;
