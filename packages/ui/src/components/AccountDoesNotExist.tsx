9c781970b92be54a5fcda38bdc533128d73a5ae6
TheMonkeyCoder <themonkeycoder@proton.me>
2024-08-28 11:15:37 +0300

    feat: improve faucet link ui

--- packages/ui/src/components/AccountDoesNotExist.tsx
+++ packages/ui/src/components/AccountDoesNotExist.tsx
@@ -1,21 +1,21 @@
 import { useMinaProvider } from "@/lib/ZkappContext";
 
 export default function AccountDoesNotExist() {
-    const { account } = useMinaProvider();
-    return (
-        <div>
-        <span className="pr-2">Account does not exist.</span>
-        <a
-            href={
-            "https://faucet.minaprotocol.com/?address=" +
-            account?.toBase58()
-            }
-            target="_blank"
-            rel="noreferrer"
-        >
-            Visit the faucet to fund this fee payer account.
-        </a>
-        </div>
-    ); }
-
-    
+  const { account } = useMinaProvider();
+  return (
+    <div>
+      <span className="pr-2">Account does not exist</span>
+      <br />
+      Visit{" "}
+      <a
+        href={"https://faucet.minaprotocol.com/?address=" + account?.toBase58()}
+        className="underline text-blue-600"
+        target="_blank"
+        rel="noreferrer"
+      >
+        the faucet
+      </a>{" "}
+      to fund this fee payer account
+    </div>
+  );
+}
